117330391aeb41b08a5f5909ed29830f6abdf3fe
Developer MoleculaX <moleculax@gmail.com>
2025-08-29 21:47:35 -0300

    mod

---
+++ mail_send.sql
@@ -0,0 +1,23 @@
+
+create database sendMail;
+select * from mail_send;
+
+
+CREATE TABLE `mail_send` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cod_proceso` varchar(10) NOT NULL DEFAULT '' COMMENT 'Nombre del proceso en cuestion, ejemplo COBRO, CANCELACION',
+  `email` varchar(150) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `asunto` varchar(200) NOT NULL DEFAULT '',
+  `cuerpo` text DEFAULT NULL,
+  `estado` char(1) NOT NULL DEFAULT '' COMMENT 'N=No eenviado/E=Enviado',
+  `fecha_ing` date DEFAULT NULL,
+  `hora_ing` time DEFAULT NULL,
+  `fecha_envio` date DEFAULT NULL,
+  `hora_envio` time DEFAULT NULL,
+  `origen` varchar(20) NOT NULL DEFAULT '' COMMENT 'Ej: CALL / ATC / ADMINISTRACION ',
+  `revisado` int(1) unsigned NOT NULL DEFAULT 0 COMMENT '[ 0 / 1 ] 0=NO Revisado / 1=SI Revisado',
+  PRIMARY KEY (`id`),
+  KEY `kfechahora_ing_estado` (`fecha_ing`,`hora_ing`,`estado`) USING BTREE,
+  KEY `kestado_fechahora_ing` (`estado`,`fecha_ing`,`hora_ing`) USING BTREE,
+  KEY `kestado_id` (`estado`,`id`) USING BTREE
+);
